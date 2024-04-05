ca68b9bbc7c451e3656bd3dbd8ceb9c59510ff9e
kapoortanish74@gmail.com <kapoortanish74@gmail.com>
2024-04-05 18:17:59 +0530

    react

--- class3/app.js
+++ class3/app.js
@@ -1,2 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+//react element
+
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+console.log(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+
+const jsx_heading = < h1 id = "heading" > namsate using JSX < /h1>;
+console.log(jsx_heading);
+const rootjsx = ReactDOM.createRoot(document.getElementById("root_jsx"));
+rootjsx.render(jsx_heading);
